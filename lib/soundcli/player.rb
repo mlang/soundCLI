d4eaf0b005e1e439574bc52d69f5371d7f1983b2
Mario Lang <mlang@delysid.org>
2014-10-12 14:42:35 +0200

    Initial conversion to gstreamer 1.0 API.
    Single playback works now, resetting the URI breaks the pipe.

--- lib/soundcli/player.rb
+++ lib/soundcli/player.rb
@@ -4,11 +4,10 @@ require "soundcli/helpers"
 
 class Player
   def initialize
-    # create the playbin
-    @playbin = Gst::ElementFactory.make("playbin2")
+    # create the bin
+    @bin, error = Gst.parse_launch("souphttpsrc name=httpsrc ! decodebin ! audioconvert ! audioresample ! autoaudiosink")
     #watch the bus for messages
-    bus = @playbin.bus
-    bus.add_watch do |bus, message|
+    @bin.bus.add_watch do |bus, message|
       handle_bus_message(message)
     end
   end
@@ -17,8 +16,7 @@ class Player
     @comments = comments
     @comment_ptr = 0
 
-    @playbin.set_property("buffer-size", Settings::all['buffer-size'])
-    @playbin.set_property("uri",uri)
+    @bin.get_by_name("httpsrc").location = uri
   end
 
   protected
@@ -38,47 +36,35 @@ class Player
 
   # get position of the playbin
   def position
-    begin
-      @query_position = Gst::QueryPosition.new(Gst::Format::TIME)
-      @playbin.query(@query_position)
-      pos = @query_position
-    rescue
-      pos = 0
-    end
+    result, pos = @bin.query_position(Gst::Format::TIME)
     return pos
   end
 
   # get song duration
   def duration
-    begin
-      @query_duration = Gst::QueryDuration.new(Gst::Format::TIME)
-      @playbin.query(@query_duration)
-      pos = @query_duration
-    rescue
-      pos = 0
-    end
-    return pos
+    result, dur = @bin.query_duration(Gst::Format::TIME)
+    return dur
   end
 
   public
   #set or get the volume
   def volume(v)
-    @playbin.set_property("volume", v) if v and (0..1).cover? v
-    return @playbin.get_property("volume")
+    @bin.set_property("volume", v) if v and (0..1).cover? v
+    return @bin.get_property("volume")
   end
 
   def quit
-    @playbin.stop
+    @bin.stop
     @mainloop.quit
   end
 
   def play
-    @playbin.play
+    @bin.play
 
     GLib::Timeout.add(100) do
-      @duration = self.ns_to_str(self.duration.parse[1])
-      @position = self.ns_to_str(self.position.parse[1])
-      timestamp = self.position.parse[1]/1000000
+      @duration = self.ns_to_str(self.duration)
+      @position = self.ns_to_str(self.position)
+      timestamp = self.position/1000000
 
       if self.playing?
         Helpers::say("#{@position}/#{@duration}  \r", :normal)
@@ -116,20 +102,20 @@ class Player
   end
 
   def resume
-    @playbin.set_state(Gst::State::PLAYING)
-    @playbin.play
+    @bin.set_state(Gst::State::PLAYING)
+    @bin.play
   end
 
   def pause
-    @playbin.set_state(Gst::State::PAUSED)
-    @playbin.pause
+    @bin.set_state(Gst::State::PAUSED)
+    @bin.pause
     Helpers::say("--- PAUSED ---\r", :normal)
   end
 
   def handle_bus_message(msg)
     case msg.type
-    when Gst::Message::Type::BUFFERING
-      buffer = msg.parse
+    when Gst::MessageType::BUFFERING
+      buffer = msg.parse_buffering
       if buffer < 100
         Helpers::say("Buffering: #{buffer}%  \r", :normal)
         self.pause if self.playing?
@@ -139,26 +125,26 @@ class Player
       end
 
       $stdout.flush
-    when Gst::Message::Type::ERROR
-      @playbin.set_state(Gst::State::NULL)
-      $stderr.puts msg.parse
+    when Gst::MessageType::ERROR
+      @bin.set_state(Gst::State::NULL)
+      $stderr.puts msg.parse_error
       self.quit
-    when Gst::Message::Type::EOS
-      @playbin.set_state(Gst::State::NULL)
+    when Gst::MessageType::EOS
+      @bin.set_state(Gst::State::NULL)
       self.quit
     end
     true
   end
 
   def done?
-    return (@playbin.get_state[1] == Gst::State::NULL)
+    return (@bin.get_state(Gst::CLOCK_TIME_NONE)[1] == Gst::State::NULL)
   end
 
   def playing?
-    return (@playbin.get_state[1] == Gst::State::PLAYING)
+    return (@bin.get_state(Gst::CLOCK_TIME_NONE)[1] == Gst::State::PLAYING)
   end
 
   def paused?
-    return (@playbin.get_state[1] == Gst::State::PAUSED)
+    return (@bin.get_state(Gst::CLOCK_TIME_NONE)[1] == Gst::State::PAUSED)
   end
 end
